dfa567b9384725f4ee0fc481896b7294b64ba8c2
David--Cléris Timothée <timothee.davidcleri@gmail.com>
2025-03-20 01:55:49 +0100

    [Ubuntu22] add new images (#1)
    * add ubuntu 22 images
    
    * add ubuntu 22 images to ci
    
    * add ubuntu 22 images to ci
    
    * add ubuntu 22 images to ci
    
    * add ubuntu 22 images to ci
    
    * fix ?
    
    * add retries option to apt
    
    * update dpcpp
    
    * update dpcpp
    
    * change dpcpp version in cuda images
    
    * change ninja call
    
    * try
    
    * try
    
    * try with a more recent release
    
    * add cuda 12
    
    * update container names
    
    * update container names
    
    * add cuda 12 dpcpp image
    
    * use older dpcpp for cuda
    
    * remove cuda11 dpcpp

---
+++ images/ubuntu22dpcpp_cuda12/install_cuda.sh
@@ -0,0 +1,5 @@
+mkdir -p /opt/cuda
+wget --progress=bar:force -O cuda.sh https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda_12.8.1_570.124.06_linux.run
+sudo sh ./cuda.sh --override --silent --toolkit --no-man-page --no-drm --no-opengl-libs --installpath=/opt/cuda || true
+echo "CUDA Version 12.8.1" | sudo tee /opt/cuda/version.txt
+rm cuda.sh

97f4194567824f2cd6e1cde6d882e029c073b1d2
David--Cléris Timothée <timothee.davidcleri@gmail.com>
2025-11-27 21:39:18 +0100

    add llvm 18 image (#6)
    * add llvm 18 image
    
    * add a few others
    
    * cleaning

---
+++ images/ubuntu24_llvm18/install_llvm.sh
@@ -0,0 +1,14 @@
+set -eu
+
+LLVM_VERSION=18
+
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh $LLVM_VERSION
+sudo apt install -y libclang-${LLVM_VERSION}-dev clang-tools-${LLVM_VERSION} libomp-${LLVM_VERSION}-dev
+
+# special case for LLVM 16
+if [[ "${LLVM_VERSION}" == "16" ]]; then
+    sudo rm -r /usr/lib/clang/16*
+    sudo ln -s /usr/lib/llvm-16/lib/clang/16 /usr/lib/clang/16
+fi
